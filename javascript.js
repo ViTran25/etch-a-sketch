ba62421366e8b6d85458e3c842cf4727b1c071e7
Vi Tran <77949259+ViTran25@users.noreply.github.com>
2023-01-04 18:57:49 -0500

    Create mouse-hovering effect
    Create 'createHoverEffect()' function. The function will add
    3 event listener to the div: 'mousedown', 'mousemove' and 'mouseup'.
    The effect will take place once the mouse is being depressed
    ('mousedown' event) and hovering over the divs ('mousemove' event)
    and it will end if the mouse is released ('mouseup' event).

--- javascript.js
+++ javascript.js
@@ -2,17 +2,36 @@ const divContainer = document.querySelector('.square-container');
 
 function createGrid(numberOfSquares) {
     for (let i=0; i<numberOfSquares; i++) {
-        
+        // Create the number of row
         const row = document.createElement('div');
         row.classList.add('row');
         divContainer.appendChild(row);
 
         for (let j=0; j<numberOfSquares; j++) {
+            // Create the number of column inside a row
             const square = document.createElement('div');
             square.classList.add('square');
             row.appendChild(square);
+            // Create mouse hovering effect for each square
+            createHoverEffect(square);
         }
     }
 };
 
 createGrid(16);
+
+let isDrawing = false;
+//Mouse hovering event but only when the mouse is being depressed and not being released
+function createHoverEffect(div) {
+    div.addEventListener('mousedown', function(e) {
+        isDrawing = true;
+    });
+    div.addEventListener('mousemove', function(e) {
+        if (isDrawing) {
+            div.style.backgroundColor = 'brown';
+        }
+    });
+    div.addEventListener('mouseup', function(e) {
+        if (isDrawing) isDrawing = false;
+    });
+}
